e2bad5daa39178031e00d0f4e50cc53f2ebe0c5d
Priyatham Kattakinda <priyathamkat@gmail.com>
2025-02-11 18:07:32 -0500

    Serve embeddings with `jina-embeddings-v3`

--- wiki_rag/servers/embeddings.py
+++ wiki_rag/servers/embeddings.py
@@ -1,8 +1,34 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import AutoModel
 
-app = FastAPI()
+embedding_model = []
 
 
-@app.get("/embeddings/{query}")
-def get_embeddings(query: str):
-    return {"response": f"Embeddings for {query}"}
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    # Load embedding model
+    model = AutoModel.from_pretrained("jinaai/jina-embeddings-v3", trust_remote_code=True)
+    embedding_model.append(model)
+    yield
+    # Unload embedding model
+    embedding_model.clear()
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+class EmbeddingsRequest(BaseModel):
+    texts: list[str]
+
+
+class Embeddings(BaseModel):
+    embeddings: list[list[float]]
+
+
+@app.post("/embeddings/", response_model=Embeddings)
+async def get_embeddings(request: EmbeddingsRequest) -> Embeddings:
+    embeddings = embedding_model[0].encode(request.texts, task="text-matching")
+    return Embeddings(embeddings=embeddings.tolist())
